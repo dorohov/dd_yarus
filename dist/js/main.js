d5f11ef84086e9d5442457fc283c91c9abef89e0
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-06 17:32:21 +0300

    init

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+    })
+})(jQuery);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oJCkge1xyXG4gICAgXCJ1c2Ugc3RyaWN0XCJcclxuICAgICQoZnVuY3Rpb24oKSB7XHJcblxyXG4gICAgfSlcclxufSkoalF1ZXJ5KTsiXX0=
