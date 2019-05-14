f0eab784bdece1dab346b3b8b9afde2db9d853c5
Pavel Proyavin <pavel@dorohovdesign.com>
2019-05-14 09:09:09 +0300

    Changelog from 130519

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 (function($) {
     "use strict"
     $(function() {
-
+        $('.__dd__block-two__ba__block').cocoen();
     })
 })(jQuery);
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oJCkge1xyXG4gICAgXCJ1c2Ugc3RyaWN0XCJcclxuICAgICQoZnVuY3Rpb24oKSB7XHJcblxyXG4gICAgfSlcclxufSkoalF1ZXJ5KTsiXX0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oJCkge1xyXG4gICAgXCJ1c2Ugc3RyaWN0XCJcclxuICAgICQoZnVuY3Rpb24oKSB7XHJcbiAgICAgICAgJCgnLl9fZGRfX2Jsb2NrLXR3b19fYmFfX2Jsb2NrJykuY29jb2VuKCk7XHJcbiAgICB9KVxyXG59KShqUXVlcnkpOyJdfQ==
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 (function($) {
     "use strict"
     $(function() {
-
+        $('.__dd__block-two__ba__block').cocoen();
     })
 })(jQuery);
